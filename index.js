18284366fc893c16b918b2d5c26fd1f5958e5339
janWICKEDpro <janroyal45@gmail.com>
2023-12-26 07:39:43 +0100

    send http request from input url and log status code

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
 const prompt = require('prompt-sync')({sigint: true});
 const inputValidator = require('./validateInput');
-const https = require('https');
+const axios = require('axios');
+
  async function main() {
     let command  = ''
-      
-   
+    
     do {
        command = prompt('%'); 
-       const  res = inputValidator(command).valid;
-        if(!res){
+        const validator = inputValidator(command);
+        const isValid = validator.valid;
+        if(!isValid){
             console.log(`${command} is not recognized \n`)
         } else{
-            console.log('at ')
-         let dat1a =   https.get('https://www.google.com', res =>{
-
-        console.log(res.data);
-        let data = '';
-
-        res.on('data', (chunk) => {
-          data += chunk;
-        });
-  
-        res.on('end', () => {
-          // Process the response data here
-          console.log(data);
-        });
-        req.on('error', (error) => {
-            console.error('Error fetching data:', error);
-          });
-            dat1a.end();
-      });
-  
-     
+         let response = await axios.default.get(validator.arr[1]);
+         console.log( 'Status Code:' + response.status);
          }
-    } while (1);
-    
-
+    } while (true);
 
 }
 
